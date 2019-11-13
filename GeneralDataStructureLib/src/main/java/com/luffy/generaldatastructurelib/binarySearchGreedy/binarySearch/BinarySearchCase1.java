e2401fa25adf3f270bb4e75092eba6f513cc752e
lvlufei <lvlufei@aoji.cn>
2019-11-13 15:58:25 +0800

    二分搜索:优化理解；增加案例

--- GeneralDataStructureLib/src/main/java/com/luffy/generaldatastructurelib/binarySearchGreedy/binarySearch/BinarySearchCase1.java
+++ GeneralDataStructureLib/src/main/java/com/luffy/generaldatastructurelib/binarySearchGreedy/binarySearch/BinarySearchCase1.java
@@ -4,91 +4,179 @@ package com.luffy.generaldatastructurelib.binarySearchGreedy.binarySearch;
  * Created by lvlufei on 2019/11/12
  *
  * @name 二分搜索
- * @desc 案例：搜索旋转排序数组
+ * @desc 例题分析一：找确定的边界
  * <p>
- * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
- * (例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2])。
- * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ * 边界分上边界和下边界，有时候也被成为右边界和左边界。确定的边界指：边界的数值等于要找的目标数。
  * <p>
- * 1，你可以假设数组中不存在重复的元素。
- * 2，你的算法时间复杂度必须是 O(log n) 级别。
+ * 题目：在一个排好序的数组中找出某个数第一次出现和最后一次出现的下标位置。
  * <p>
  * 示例：
- * 输入: nums = [4,5,6,7,0,1,2], target = 0
- * 输出: 4
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: [3,4]
  */
 public class BinarySearchCase1 {
+
     /**
-     * @param nums   数组数据
+     * 二分搜索-递归解法
+     *
+     * @param nums   数据数组
      * @param target 目标值
+     * @param left   左边索引（起点）
+     * @param right  右边索引（终点）
      * @return
      */
-    public static int search(int[] nums, int target) {
-        int n = nums.length;
-        if (n == 0)
+    public static int[] binarySearchRecursion(int[] nums, int target, int left, int right) {
+        int[] bounds = new int[2];
+        bounds[0] = searchLeftBoundRecursion(nums, target, left, right);
+        bounds[1] = searchRightBoundRecursion(nums, target, left, right);
+        return bounds;
+    }
+
+    /**
+     * 搜索左边界（下边界）-递归解法
+     *
+     * @param nums   数组数据
+     * @param target 目标值
+     * @param left   左边索引（起点）
+     * @param right  右边索引（终点）
+     * @return 返回目标值所在数组索引
+     */
+    private static int searchLeftBoundRecursion(int[] nums, int target, int left, int right) {
+        //为了避免无限循环，先判断，如果起点位置大于终点位置，表明超出了搜索区间，无法找到目标数，返回-1。
+        if (left > right) {
             return -1;
-        if (n == 1)
-            return nums[0] == target ? 0 : -1;
-        int rotate_index = find_rotate_index(nums, 0, n - 1);
-        if (nums[rotate_index] == target)
-            return rotate_index;
-        if (rotate_index == 0)
-            return search(nums, target, 0, n - 1);
-        if (target < nums[0])
-            return search(nums, target, rotate_index, n - 1);
-        return search(nums, target, 0, rotate_index);
-    }
-
-    /**
-     * @param nums  数组数据
-     * @param left  左边索引
-     * @param right 右边索引
-     * @return
-     */
-    private static int find_rotate_index(int[] nums, int left, int right) {
-        if (nums[left] < nums[right])
-            return 0;
-        while (left <= right) {
-            int pivot = (left + right) / 2;
-            if (nums[pivot] > nums[pivot + 1])
-                return pivot + 1;
-            else {
-                if (nums[pivot] < nums[left])
-                    right = pivot - 1;
-                else
-                    left = pivot + 1;
-            }
         }
-        return 0;
+        //取中位数。
+        int middle = left + (right - left) / 2;
+        //判断中位数是否为要找的数，如果是，就返回中位数下标。
+        if (nums[middle] == target && (middle == 0 || nums[middle - 1] < target)) {
+            return middle;
+        }
+        //如果目标值在左边，就从左半边递归地进行二分搜索；否则从右半边递归地进行二分搜索。
+        if (target <= nums[middle]) {
+            return searchLeftBoundRecursion(nums, target, left, middle - 1);
+        } else {
+            return searchLeftBoundRecursion(nums, target, middle + 1, right);
+        }
     }
 
     /**
+     * 搜索右边界（上边界）-递归解法
+     *
      * @param nums   数组数据
      * @param target 目标值
-     * @param left   左边索引
-     * @param right  右边索引
+     * @param left   左边索引（起点）
+     * @param right  右边索引（终点）
+     * @return 返回目标值所在数组索引
+     */
+    private static int searchRightBoundRecursion(int[] nums, int target, int left, int right) {
+        //为了避免无限循环，先判断，如果起点位置大于终点位置，表明超出了搜索区间，无法找到目标数，返回-1。
+        if (left > right) {
+            return -1;
+        }
+        //取中位数。
+        int middle = left + (right - left) / 2;
+        //判断中位数是否为要找的数，如果是，就返回中位数下标。
+        if (nums[middle] == target && (middle == nums.length - 1 || nums[middle + 1] > target)) {
+            return middle;
+        }
+        //如果目标值在左边，就从左半边递归地进行二分搜索；否则从右半边递归地进行二分搜索。
+        if (target < nums[middle]) {
+            return searchRightBoundRecursion(nums, target, left, middle - 1);
+        } else {
+            return searchRightBoundRecursion(nums, target, middle + 1, right);
+        }
+    }
+
+    /**
+     * 二分搜索-非递归解法
+     *
+     * @param nums   数据数组
+     * @param target 目标值
+     * @param left   左边索引（起点）
+     * @param right  右边索引（终点）
      * @return
      */
-    private static int search(int[] nums, int target, int left, int right) {
+    public static int[] binarySearch(int[] nums, int target, int left, int right) {
+        int[] bounds = new int[2];
+        bounds[0] = searchLeftBound(nums, target, left, right);
+        bounds[1] = searchRightBound(nums, target, left, right);
+        return bounds;
+    }
+
+    /**
+     * 搜索左边界（下边界）-非递归解法
+     *
+     * @param nums   数组数据
+     * @param target 目标值
+     * @param left   左边索引（起点）
+     * @param right  右边索引（终点）
+     * @return 返回目标值所在数组索引
+     */
+    private static int searchLeftBound(int[] nums, int target, int left, int right) {
+        //在while循环里，判断搜索的区间范围是否有效
         while (left <= right) {
-            int pivot = (left + right) / 2;
-            if (nums[pivot] == target)
-                return pivot;
-            else {
-                if (target < nums[pivot])
-                    right = pivot - 1;
-                else
-                    left = pivot + 1;
+            //取中位数
+            int middle = left + (right - left) / 2;
+            //判断中位数是否为要找的数，如果是，就返回中位数下标。
+            if (nums[middle] == target && (middle == 0 || nums[middle - 1] < target)) {
+                return middle;
+            }
+            //如果目标值在左边，调整搜索区间的终点为"middle - 1"；否则，调整搜索区间的起点为"middle + 1"。
+            if (target <= nums[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
             }
         }
+        //如果超出了搜索区间，表明无法找到目标数，返回-1。
+        return -1;
+    }
+
+    /**
+     * 搜索右边界（上边界）-非递归解法
+     *
+     * @param nums   数组数据
+     * @param target 目标值
+     * @param left   左边索引（起点）
+     * @param right  右边索引（终点）
+     * @return 返回目标值所在数组索引
+     */
+    private static int searchRightBound(int[] nums, int target, int left, int right) {
+        //在while循环里，判断搜索的区间范围是否有效
+        while (left <= right) {
+            //取中位数
+            int middle = left + (right - left) / 2;
+            //判断中位数是否为要找的数，如果是，就返回中位数下标。
+            if (nums[middle] == target && (middle == nums.length - 1 || nums[middle + 1] > target)) {
+                return middle;
+            }
+            //如果目标值在左边，调整搜索区间的终点为"middle - 1"；否则，调整搜索区间的起点为"middle + 1"。
+            if (target < nums[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        //如果超出了搜索区间，表明无法找到目标数，返回-1。
         return -1;
     }
 
     public static void main(String[] args) {
-        int[] nums = {4, 5, 6, 7, 0, 1, 2};
-        int target = 0;
-        int result = search(nums, target);
-        System.out.print(result);
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        //递归解法
+        int[] resultIndexRecursion = binarySearchRecursion(nums, target, 0, nums.length - 1);
+        System.out.print("二分搜索-递归解法:");
+        for (int i : resultIndexRecursion) {
+            System.out.print(i + "    ");
+        }
+        //非递归解法
+        int[] resultIndex = binarySearch(nums, target, 0, nums.length - 1);
+        System.out.print("\n二分搜索-非递归解法:");
+        for (int i : resultIndex) {
+            System.out.print(i + "    ");
+        }
     }
 
 }
--- GeneralDataStructureLib/src/main/java/com/luffy/generaldatastructurelib/binarySearchGreedy/binarySearch/BinarySearchCase2.java
+++ GeneralDataStructureLib/src/main/java/com/luffy/generaldatastructurelib/binarySearchGreedy/binarySearch/BinarySearchCase2.java
@@ -3,39 +3,20 @@ package com.luffy.generaldatastructurelib.binarySearchGreedy.binarySearch;
 /**
  * Created by lvlufei on 2019/11/12
  *
- * @name 二分搜索（折半搜索）
- * @desc --
+ * @name 二分搜索
+ * @desc 例题分析二：找模糊的边界
  * <p>
- * 定义：
- * 是一种在有序数组中查找某一特定元素的搜索算法。从定义可知，运用二分搜索的前提是数组必须是排好序的。另外，输入并不一定是数组，也有可能是给定一个区间的起始和终止的位置。
+ * 二分搜索可以用来查找一些模糊的边界。模糊的边界指：边界的值并不等于目标的值，而是大于或者小于目标的值。
  * <p>
- * 优点：时间复杂度是O(lgn)，非常高效。因此也成为对数搜索。
- * 缺点：要求待查找的数组或者区间是排好序的。
+ * 题目：从数组 {-2,0,1,4,7,9,10} 中找到第一个大于6的数。
  * <p>
  * 解题思路：
- * <p>
- * 1，从已经排好序的数组或区间中取出中间位置的元素，判断该元素是否满足要搜索的条件，如果满足，停止搜索，程序结束。
- * 2，如果正中间的元素不满足条件，则从它两边的区域进行搜索。由于数组是排好序的，可以利用排除法，确定接下来应该从这两个区间中的哪一个去搜索。
- * 3，通过判断，如果发现真正要找的元素在左半区间的话，就继续在左半区间里进行二分搜索。反之，就在右半区间里进行二分搜索。
- * <p>
- * 核心步骤:
- * 1，确定搜索的范围和区间
- * 2，取中间的数判断是否满足条件
- * 3，如果不满足条件，判定应该往哪个半边继续进行搜索
- * <p>
- * 题目：假设我们要从一个排好序的数组里 {1, 3, 4, 6, 7, 8, 10, 13, 14} 查看一下数字 7 是否在里面，如果在，返回它的下标，否则返回 -1。
- * <p>
- * 解题方法：递归解法、非递归解法
- * <p>
- * 递归解法：
- * 优点：简洁；缺点：执行消耗大
- * <p>
- * 注意：
- * 1，在计算 middle 下标的时候，不能简单地用 (left + right) / 2，可能会导致溢出。
- * 2，在取左半边以及右半边的区间时，左半边是 [left, middle - 1]，右半边是 [middle + 1, right]，这是两个闭区间。因为已经确定了 middle 那个点不是我们要找的，就没有必要再把它加入到左、右半边了。
- * 3，对于一个长度为奇数的数组，例如：{1, 2, 3, 4, 5}，按照 left + (right - left) / 2 来计算，middle 就是正中间的那个位置，对于一个长度为偶数的数组，例如 {1, 2, 3, 4}，middle 就是正中间靠左边的一个位置。
+ * 在一个排好序的数组里，判断一个数是不是第一个大于 6 的数，只要它满足如下的条件：
+ * 1，该数要大于6；
+ * 2，该数有可能是数组里的第一个数，或者它之前的一个数比6小。
+ * 只要满足了上面的条件就是第一个大于6的数。
  */
-public class BinarySearchApply {
+public class BinarySearchCase2 {
 
     /**
      * 二分搜索-递归解法
@@ -54,16 +35,17 @@ public class BinarySearchApply {
         //取中位数。
         int middle = left + (right - left) / 2;
         //判断中位数是否为要找的数，如果是，就返回中位数下标。
-        if (nums[middle] == target) {
+        if (nums[middle] > target && (middle == 0 || nums[middle - 1] <= target)) {
             return middle;
         }
         //如果目标值在左边，就从左半边递归地进行二分搜索；否则从右半边递归地进行二分搜索。
         if (target < nums[middle]) {
-            return binarySearch(nums, target, left, middle - 1);
+            return binarySearchRecursion(nums, target, left, middle - 1);//在左半边继续进行二分搜索
         } else {
-            return binarySearch(nums, target, middle + 1, right);
+            return binarySearchRecursion(nums, target, middle + 1, right);//在右半边继续进行二分搜索
         }
     }
+
 
     /**
      * 二分搜索-非递归解法
@@ -80,7 +62,7 @@ public class BinarySearchApply {
             //取中位数
             int middle = left + (right - left) / 2;
             //判断中位数是否为要找的数，如果是，就返回中位数下标。
-            if (nums[middle] == target) {
+            if (nums[middle] > target && (middle == 0 || nums[middle - 1] <= target)) {
                 return middle;
             }
             //如果目标值在左边，调整搜索区间的终点为"middle - 1"；否则，调整搜索区间的起点为"middle + 1"。
@@ -95,8 +77,8 @@ public class BinarySearchApply {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 3, 4, 6, 7, 8, 10, 13, 14};
-        int target = 7;
+        int[] nums = {-2, 0, 1, 4, 7, 9, 10};
+        int target = 6;
         //递归解法
         int resultIndexRecursion = binarySearchRecursion(nums, target, 0, nums.length - 1);
         System.out.println("二分搜索-递归解法：" + resultIndexRecursion);
@@ -104,4 +86,5 @@ public class BinarySearchApply {
         int resultIndex = binarySearch(nums, target, 0, nums.length - 1);
         System.out.println("二分搜索-非递归解法：" + resultIndex);
     }
+
 }
